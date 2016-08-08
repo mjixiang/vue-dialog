4146df513053e5db0a50758afa51dbc8ae796784
max <820396305@qq.com>
2016-08-08 09:55:28 +0800

    create

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # vue-dialog
 A drag-able dialog for Vue.js
+
+基于Vue.js的弹框组件
+
+[查看jquery版本](https://github.com/mjixiang/ui.prompt)
+
+##example
+地址：https://mjixiang.github.io/vue-dialog
+
+
+##props
+```html
+<dialog :show.sync="dialog.show" :title="dialog.title" :buttons="dialog.buttons">
+	<!--窗体主体内容-->
+</dialog>
+```
+
+>`show`:`Boolean` 是否显示，必须，双向绑定
+
+>`title`:`Object || String` 弹框标题，需要，自定义弹框标题和样式
+
+>`buttons`:`Array` 按钮，可选，自定义弹框下方按钮样式、文本、事件
+
+##data
+```javascript
+data: {
+	dialog:{
+		show:false,
+		title:{
+			text:'这个是标题（可拖拽）',
+			className:'xa-bg-red'	//标题样式类名，包含`background`、`color`即可
+		},
+		buttons:[
+			/**
+				`@text`：按钮文字
+				`@className`：按钮样式类名，包含`background`、`color`即可（内置一部分）
+				`@clickEvent`：按钮点击事件后监听的事件名
+			*/
+			{text:'关闭',className:'xa-bg-red-click',clickEvent:'cancelEvent'},
+			{text:'确定',className:'xa-bg-blue-click',clickEvent:'confirmEvent'}
+		]
+	}
+}
+```
+
+##events
+```javascript
+events:{
+	//buttons中定义的所有的`clickEvent`
+	cancelEvent(){
+  		this.dialog.show = false;
+	},
+	confirmEvent(){
+		//do something
+	}
+}
+```
---
+++ dialog.vue
@@ -0,0 +1,117 @@
+<style scoped>
+	/*dialog*/
+	.xa-popup .layer{position:fixed;left:0;top:0;background:rgba(0,0,0,0.3);width:100%;height:100%;z-index:100;}
+	.xa-popup .popup{min-width:300px;background:#fff;z-index:101;position:fixed;left:50%;top:50%;box-shadow:0 0 10px #888;border-radius:5px;overflow:hidden;}
+	.xa-popup .popup>.title{width:100%;height:44px;line-height:44px;text-align:center;font-size:16px;cursor:move;position:relative;}
+	.xa-popup .popup>.title>.close{height:44px;width:44px;position:absolute;right:0;top:0;cursor:pointer;font-size:30px;}
+	.xa-popup .popup>.title>.close:after{content:'×';display:block;}
+	.xa-popup .popup>.title>.close:hover{background:rgba(255,255,255,0.1);}
+	.xa-popup .popup>.main{overflow-y:auto;overflow-x:hidden;}
+	.xa-popup .popup>.buttons{display:flex;justify-content:center;padding:10px 10px;box-shadow:0 0 6px #ccc;}
+	.xa-popup .popup>.buttons .button{flex:1;max-width:160px;text-align:center;cursor:pointer;height:36px;line-height:36px;box-shadow:0 0 1px #ccc;margin:0 10px;border-radius:5px;}
+	/* theme库 */
+	.xa-bg-eee{background:#eee;color:#333;}
+	.xa-bg-red,.xa-bg-red-click{background-color:#F33A3A;color:white;}
+	.xa-bg-orange,.xa-bg-orange-click{background-color:#FA9E3B;color:white;}
+	.xa-bg-yellow,.xa-bg-yellow-click{background-color:#fff600;color:white;}
+	.xa-bg-green,.xa-bg-green-click{background-color:#009000;color:white;}
+	.xa-bg-blue,.xa-bg-blue-click{background-color:#268dff;color:white;}
+	.xa-bg-gray,.xa-bg-gray-click{background-color:#BCBCBC;}
+	.xa-bg-white,.xa-bg-white-click{background-color:white;color:#333;}
+	.xa-bg-black,.xa-bg-black-click{background-color:black;color:white;}
+
+	.xa-bg-red-click:hover{background-color:#EF4F4F;}
+	.xa-bg-orange-click:hover{background-color:#FAAE4B;}
+	.xa-bg-yellow-click:hover{background-color:#ffff00;}
+	.xa-bg-green-click:hover{background-color:#009900;}
+	.xa-bg-blue-click:hover{background-color:#007ae2;}
+	.xa-bg-gray-click:hover{background-color:#BCB6BC;}
+	.xa-bg-white-click:hover{background-color:#f2f2f2;}
+	.xa-bg-black-click:hover{background-color:#333;}
+	.xa-click:hover{background:#f2f2f2;}
+</style>
+<template>
+	 <div v-show="show" class="xa-popup">
+	  	<div class="layer"></div>
+	  	<div class="popup">
+	  		<div :class="title.className || 'xa-bg-eee'" class="title">
+	  			<div class="name">{{ title.name || title }}</div>
+	  			<div @click="show = false" class="close"></div>
+	  		</div>
+	  		<div class="main"><slot></slot></div>
+			<div v-show="buttons.length" class="buttons">
+				<div v-for="btn in buttons" @click="setEvent(btn.clickEvent)" :class="btn.className" class="button">{{ btn.text }}</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+var drag = (dragBox,moveBox = dragBox) => {
+    dragBox.onmousedown = e => {
+        var disX=e.clientX-moveBox.offsetLeft;
+        var disY=e.clientY-moveBox.offsetTop;
+        document.onmousemove = (e) =>{
+            e.preventDefault();
+            var l=e.clientX-disX;
+            var t=e.clientY-disY;
+            var x = document.documentElement.clientWidth-moveBox.offsetWidth;
+            var y = document.documentElement.clientHeight-moveBox.offsetHeight;
+            l = l < 0 ? 0 : (l > x ? x : l);
+            t = t < 0 ? 0 : (t > y ? y : t);
+            moveBox.style.left=l+'px';
+            moveBox.style.top=t+'px';
+            return false;
+        }
+        document.onmouseup = () => {
+            document.onmousemove = null;    
+            document.onmouseup = null;
+            return false;
+        }
+        return false;
+    }
+}
+export default {
+	methods:{
+		// 弹框适配屏幕居中
+		autosize(){
+			var dom = this.$el.querySelector('.popup');
+            var CHeight = document.documentElement.clientHeight;
+            var CWidth = document.documentElement.clientWidth;
+            var PHeight = dom.offsetHeight;
+            var PWidth = dom.offsetWidth;
+            dom.querySelector(".main").style.maxHeight = CHeight - 100 +'px';
+            dom.style.top = (CHeight>PHeight?(CHeight-PHeight)/2:0) +'px';
+            dom.style.left = (CWidth-PWidth)/2 +'px';
+        },
+        setEvent(eventName){
+        	this.$dispatch(eventName)
+        }
+    },
+	props:{
+		title:{
+			default:''
+		},
+		show:{
+			type:Boolean,
+			default:false,
+			twoWay:true
+		},
+		buttons:{
+			type:Array,
+			default(){
+				return []
+			}
+		}
+	},
+	watch:{
+		show(val){
+			val && this.autosize();
+		}
+	},
+	ready() {
+		drag(this.$el.querySelector('.title'),this.$el.querySelector('.popup'));
+	}
+}
+</script>
+
